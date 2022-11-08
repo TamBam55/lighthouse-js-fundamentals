db8b4d6f40a83f8623ceb91c0df011efa3d3ca66
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-11-07 21:18:15 -0800

    Trying to learn functions

---
+++ hello.js
@@ -0,0 +1,14 @@
+//const sayHello  = function () {
+  //console.log("Hello, world");
+//}
+
+sayHello();
+//executes 'Hello, World'
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
